ae25dbaad5b76ebc5a20bf2c4f542d95d87d1d3e
Orlando Quero <orlaqp@binaryways.com>
2023-04-24 16:02:31 -0400

    added amplify hosting and auth modules

--- apps/ui/src/environments/environment.ts
+++ apps/ui/src/environments/environment.ts
@@ -3,4 +3,8 @@
 
 export const environment = {
   production: false,
+  "aws_project_region": "us-east-1",
+  "aws_content_delivery_bucket": "amplifyest-20230424155222-hostingbucket-dev",
+  "aws_content_delivery_bucket_region": "us-east-1",
+  "aws_content_delivery_url": "http://amplifyest-20230424155222-hostingbucket-dev.s3-website-us-east-1.amazonaws.com"
 };
--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -3,6 +3,10 @@ import * as ReactDOM from "react-dom/client";
 
 import App from "./app/app";
 
+import { Amplify } from 'aws-amplify';
+import { environment } from './environments/environment';
+Amplify.configure(environment);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
